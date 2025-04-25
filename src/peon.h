e9cec2a4f59a18981328ea6f2833a94af188078f
FRANCESCO GRIECO TELLERIA <f.grieco@alumnos.upm.es>
2025-04-25 10:53:11 +0200

    Implementa la lógica básica del tablero con movimiento y gravedad pero estan en disposicion horizontal, estoy probando cosas

--- src/peon.h
+++ src/peon.h
@@ -1,5 +1,13 @@
 #pragma once
-class peon
-{
-};
+#include "pieza.h"
 
+class Peon : public Pieza {//importante!! es una clase que hereda de pieza PEON en el ambito PIEZA
+public:
+	Peon(Color color) : Pieza(TipoPieza::PEON, color) {}//constructor de la clase peon, inicializa el tipo de pieza y el color
+
+    bool movimientoValido(int xIni, int yIni, int xFin, int yFin) override {
+        int dy = (color == Color::BLANCO) ? -1 : 1;//el peón se mueve una casilla hacia adelante (hacia arriba si es blanco, abajo si es negro).
+        return (xIni == xFin) && (yFin - yIni == dy);
+    }//todavía falta capturas, es un codigo INICIAL
+};
+//el override lo que hace es que el método está sobrescribiendo un método virtual que fue definido en una clase base siendo la clase base pieza.
--- src/tablero.h
+++ src/tablero.h
@@ -1,5 +1,18 @@
 #pragma once
-class tablero
-{
+#include "pieza.h"
+#include "peon.h"
+
+
+class Tablero {
+private:
+    Pieza* casillas[8][8]; //tablero 8x8
+
+public:
+    Tablero(); //constructor
+    ~Tablero();//destructor
+    void inicializar(); //se usará para poner las piezas en la posición incial, luego si queremos guardar una partida podremos modificar esta función
+    void mostrar(); // solo consola
+    bool mover(int xIni, int yIni, int xFin, int yFin);// para mover las piezas
+    void aplicarGravedad(int columna); //primer intento de gravedad
 };
 
